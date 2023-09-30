f3d7786a5251e05159e01301e764fbcc0cfb5e05
Joao Brito <joao.brito@holos.company>
2023-09-30 12:50:34 -0300

    fix: fixed get_docs_by_domain_and_date()

--- src/_drv_mongodb.py
+++ src/_drv_mongodb.py
@@ -5,10 +5,9 @@ v.2023-07-29
 """
 import os
 
-from datetime import datetime
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
+from pymongo.errors import PyMongoError
 
 
 class MongoCnx():
@@ -39,63 +38,79 @@ class MongoCnx():
         Returns:
         - HTML page content.
         """
-        # Upload results to Mongodb, overwrite duplicates
-        collection = self.db[collection]
+        try:
+            # Upload results to Mongodb, overwrite duplicates
+            collection = self.db[collection]
 
-        new_documents_count = 0
-        updated_documents_count = 0
+            new_documents_count = 0
+            updated_documents_count = 0
 
-        for document in document_list:
-            filter_condition = {"_id": document["_id"]}
-            update_data = {"$set": document}
-            update_result = collection.update_one(filter_condition, update_data, upsert=True)
+            for document in document_list:
+                filter_condition = {"_id": document["_id"]}
+                update_data = {"$set": document}
+                update_result = collection.update_one(filter_condition, update_data, upsert=True)
 
-            # Check if the document was updated or inserted
-            if update_result.matched_count > 0:
-                updated_documents_count += 1
-            else:
-                new_documents_count += 1
+                # Check if the document was updated or inserted
+                if update_result.matched_count > 0:
+                    updated_documents_count += 1
+                else:
+                    new_documents_count += 1
 
-        print(
-            f"INFO  - Inserted {new_documents_count}, updated {updated_documents_count} documents \
+            print(
+                f"INFO  - Inserted {new_documents_count}, updated {updated_documents_count} documents \
 into '{self.db.name}' collection '{collection.name}'")
 
-        return None
+            return None
+        except PyMongoError as e:
+            print("ERROR - Failure querying MongoDB: ", e)
+            raise PyMongoError
 
-    def get_urls_by_source_and_date(self, collection_name, source, start_date):
+    def get_docs_by_domain_and_date(self, collection_name, domain, start_datetime):
         """
-        Get a list of URLs from a specific source with a publish date after the given date.
+        Retrieve documents from given domain with a publish date after the given date.
 
         Args:
-        - collection_name (str): The name of the collection to query.
-        - source (str): The source to filter documents by.
-        - start_date (str): The start date in YYYY-MM-DD format.
+            collection_name (str): The name of the collection to query.
+            domain (str): The domain to filter articles by.
+            start_datetime (str): The minimum publish date for articles in ISO 8601 format.
 
         Returns:
-        - list: A list of URLs that meet the criteria.
+            list: A list of matching articles as dictionaries.
         """
         try:
-            start_datetime = datetime.strptime(start_date, "%Y-%m-%d")
-            collection = self.db[collection_name]
+
+            # Define the query
             query = {
-                "source": source,
-                "publish_date": {"$gte": start_datetime.strftime("%a, %d %b %Y %H:%M:%S %z")}
+                "domain": domain,
+                "publish_date": {"$gte": start_datetime},
             }
-            # print("DEBUG - Query:", query)
-            cursor = collection.find(query)
-            urls = [doc.get("url") for doc in cursor]
+            projection = {
+                "_id": 1,
+                "publish_date": 1,
+                "domain": 1,
+                "url": 1,
+            }
+            sort_parameter = [("publish_date", 1)]
 
-            print(f"INFO  - Returned {len(urls)} urls from '{source}' after '{start_datetime}'")
-            return urls
+            # Find and sort the documents
+            collection = self.db[collection_name]
+            query_result = collection.find(query, projection).sort(sort_parameter)
 
-        except Exception as e:
-            print("ERROR - ", e)
-            return []
+            # Convert query result to a list of dictionaries
+            documents = [document for document in query_result]
+
+            print(f"INFO  - Returned {len(documents)} urls from '{domain}' after '{start_datetime}'")
+            return documents
+
+        except PyMongoError as e:
+            print("ERROR - Failure querying MongoDB: ", e)
+            raise PyMongoError
 
 
 if __name__ == "__main__":
 
-    client = MongoCnx("news_db").client
+    mongo_cnx = MongoCnx("news_db")
+    client = mongo_cnx.client
 
     database_list = client.list_database_names()
     print(database_list)
@@ -104,6 +119,15 @@ if __name__ == "__main__":
 
     collection_list = db.list_collection_names()
     print(collection_list)
+
+    collection = "news"
+    domain = "www.wsj.com"
+    publish_date = "2023-09-28T12:00"
+
+    documents = mongo_cnx.get_docs_by_domain_and_date(collection, domain, publish_date)
+
+    for document in documents[0:2]:
+        print(document)
 
     # Close the client connection when you're done
     client.close()
