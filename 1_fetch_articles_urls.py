fa0423b87eea76656c92b4191c8b4507c34a79b1
Joao Brito <joao.brito@holos.company>
2023-10-04 21:26:01 -0300

    feat: partial fuzzy matches for keywords

--- 1_fetch_articles_urls.py
+++ 1_fetch_articles_urls.py
@@ -6,11 +6,12 @@ v.2023-10-02
 import hashlib
 import json  # noqa
 import os
-import pendulum
+from dateutil import parser
 from urllib.parse import urlparse
 
 import feedparser
 from dotenv import load_dotenv
+from rapidfuzz import fuzz
 from requests.exceptions import RequestException
 
 from src._decorators import retry
@@ -19,13 +20,13 @@ from src._drv_scrapers import CustomRequests
 
 # Load variables from .env
 load_dotenv()
-proxy_username = os.getenv('PROXY_USERNAME')
-proxy_password = os.getenv('PROXY_PASSWORD')
-proxy_server = os.getenv('PROXY_SERVER')
-proxy_port = os.getenv('PROXY_PORT')
-wsj_username = os.getenv('WSJ_USERNAME')
-wsj_password = os.getenv('WSJ_PASSWORD')
-bing_apikey = os.getenv('BING_APIKEY')
+proxy_username = os.getenv("PROXY_USERNAME")
+proxy_password = os.getenv("PROXY_PASSWORD")
+proxy_server = os.getenv("PROXY_SERVER")
+proxy_port = os.getenv("PROXY_PORT")
+wsj_username = os.getenv("WSJ_USERNAME")
+wsj_password = os.getenv("WSJ_PASSWORD")
+bing_apikey = os.getenv("BING_APIKEY")
 # print(proxy_username, proxy_password, proxy_server, proxy_port, wsj_username, wsj_password, bing_apikey, openai_apikey)  # noqa
 
 files_path = "./news_data"
@@ -33,17 +34,51 @@ if not os.path.exists(files_path):
     os.makedirs(files_path)
 
 
-def convert_to_iso_date_string(input_date, output_timezone='UTC'):
+def convert_date_string_to_iso(date_str):
+    """
+    Convert a date string to an ISO-8601 formatted date string.
+
+    Args:
+        date_str (str): The date string in a recognized format.
+
+    Returns:
+        str: An ISO-8601 formatted date string.
+
+    Supported date formats:
+        'Wed, 04 Oct 2023 20:00:00 GMT' and 'Wed, 04 Oct 2023 20:00:00 -0400'
+    """
     try:
-        parsed_date = pendulum.from_format(input_date, 'ddd, DD MMM YYYY HH:mm:ss ZZ')
-        converted_date = parsed_date.in_tz(output_timezone)
-        formatted_date = converted_date.to_iso8601_string()
+        date_obj = parser.parse(date_str)
+        iso_date_str = date_obj.isoformat()
 
-        return formatted_date
+    except ValueError as e:
+        raise ValueError(f"Invalid date format: {str(e)}.")
 
-    except ValueError:
-        print("ERROR - Invalid datetime string passed as input")
-        return None
+    return iso_date_str
+
+
+def filter_news_dict(news_obj_list, keyword_list):
+    # Filter articles with keywords
+    print(f"INFO  - Filtering {len(news_obj_list)} items for keywords.")
+    filtered_list = []
+
+    for news_item in news_obj_list:
+        title = news_item.get("title", "").lower()  # Convert title to lowercase
+
+        for item in keyword_list:
+            keyword = item["keyword"].lower()
+            threshold = item.get("match_threshold", 80)
+            # print("DEBUG - ", keyword, threshold, title)
+
+            if fuzz.partial_ratio(keyword, title) >= threshold:
+                news_item['keyword'] = keyword  # Add the matching keyword to news_item
+                filtered_list.append(news_item)
+                print(f"INFO  - Keyword '{keyword}' match on '{title}'")
+                break  # Exit the loop once a match is found
+
+    print(f"INFO  - Found {len(filtered_list)} matches.")
+
+    return filtered_list
 
 
 @retry()
@@ -61,7 +96,6 @@ def request_bing_news_urls(session, query, results_count=100, freshness="day"):
         - list: List of all 'entry.link' values.
     """
     endpoint = "https://api.bing.microsoft.com/v7.0/news/search"
-
     params = {
         "q": query,
         # "category": category,
@@ -70,12 +104,8 @@ def request_bing_news_urls(session, query, results_count=100, freshness="day"):
         "mkt": "en-US",
         "setLang": "en"
     }
-
+    headers = {"Ocp-Apim-Subscription-Key": bing_apikey}
     print(f"\nINFO  - Querying {endpoint} for {query}")
-
-    headers = {
-        "Ocp-Apim-Subscription-Key": bing_apikey
-    }
 
     try:
         # Make the API request
@@ -83,170 +113,159 @@ def request_bing_news_urls(session, query, results_count=100, freshness="day"):
         response.raise_for_status()
 
         response_data = response.json()
-        results = []
-        all_links = []
 
-        for value in response_data["value"]:
-            url = value.get("url", "")
+        # Build JSON with results data
+        all_entries = []
+
+        for entry in response_data['value']:
+            url = entry.get("url", "")
             url_hash = hashlib.md5(url.encode()).hexdigest()
             parsed_url = urlparse(url)
-            domain = parsed_url.netloc
+            domain = parsed_url.netloc.replace("www.", "")
 
             extracted_entry = {
                 "_id": url_hash,
-                "category": value.get("category", ""),
+                # "source": "Bing",
+                "category": entry.get("category", ""),
                 "domain": domain,
-                "publish_date": value.get("datePublished", ""),
-                "source": "Bing",
-                "title": value.get("name", ""),
+                "publish_date": entry.get("datePublished", ""),
+                "title": entry.get("name", ""),
                 "url": url,
                 "status": "fetched",
             }
 
-            results.append(extracted_entry)
-            all_links.append(value.get("url", ""))
+            all_entries.append(extracted_entry)
 
-        # Create a dictionary for the extracted data
-        result_dict = {"news": results}
+        results = all_entries
+        print(f"INFO  - Found {len(results)} entries with query '{query}'.")
 
-        print(f"INFO  - Found {len(all_links)} urls with given parameters")
-
-        return result_dict, all_links  # Return both the JSON and the list of entry.link values
+        return results
 
     except RequestException as e:
         print(f"ERROR - An error occurred during the API request: {e}")
-        return None, []
+        return None
 
 
 @retry()
-def extract_rss_article_urls(rss_feed_urls):
+def extract_rss_article_urls(handler, rss_url):
     """
-    Extracts article data from a list of RSS feed URLs.
+    Extracts article data from an RSS feed URL.
 
     Args:
-    - rss_feed_urls (list): List of RSS feed URLs.
+        handler: The session handler for making HTTP requests.
+        rss_url (str): The URL of the RSS feed.
 
     Returns:
-    - tuple: A tuple containing:
-        - dict: JSON-compatible dict containing extracted article data.
-        - list: List of all 'entry.link' values.
+        dict: JSON-compatible dict containing extracted article data.
     """
-    print(f"\nINFO  - Quering {len(rss_feed_urls)} RSS feeds for article urls")
-
-    # Initialize lists to store data
-    all_entries = []
-    all_links = []
 
     try:
-        for rss_feed_url in rss_feed_urls:
-            # Parse the RSS feed
-            feed = feedparser.parse(rss_feed_url)
+        # Use the session object to fetch the RSS feed
+        print(f"\nINFO  - Fetching articles on RSS Feed '{rss_url}'")
+        response = handler.get_response(rss_url)
+        response.raise_for_status()
 
-            if feed.get("entries"):
-                # Append the feed data to the list
-                all_entries.extend(feed.entries)
+        feed_content = response.text
+        feed = feedparser.parse(feed_content)
 
-                # Extract and append entry.link values to the list
-                all_links.extend([entry.get("link", "") for entry in feed.entries])
+        # Build JSON with results data
+        all_entries = []
 
-        extracted_data = []
-
-        for entry in all_entries:
+        for entry in feed.entries:
             # Extract MD5 hash (_id) and domain from article url
             url = entry.get("link", "")
             url_hash = hashlib.md5(url.encode()).hexdigest()
             parsed_url = urlparse(url)
-            domain = parsed_url.netloc
+            domain = parsed_url.netloc.replace("www.", "")
 
-            # Convert date from 'Sat, 30 Sep 2023 02:33:34 -0400' inti ISO format
+            # Convert date from "Sat, 30 Sep 2023 02:33:34 -0400" inti ISO format
             pubDdate = entry.get("published", "")
-            published_date = convert_to_iso_date_string(pubDdate)
+            published_date = convert_date_string_to_iso(pubDdate)
 
             # Extract article data
             extracted_entry = {
                 "_id": url_hash,
-                "category": entry.get("wsj_articletype", ""),
                 "domain": domain,
                 "publish_date": published_date,
-                "source": "WSJ",
                 "title": entry.get("title", ""),
                 "url": url,
                 "status": "fetched",
             }
-            extracted_data.append(extracted_entry)
+            all_entries.append(extracted_entry)
 
-        # Create a dictionary for the extracted data
-        result_dict = {"news": extracted_data}
+        results = all_entries
+        print(f"INFO  - Found {len(results)} entries.")
 
-        print(f"INFO  - Found {len(all_links)} URLs in the given RSS feeds")
-
-        return result_dict, all_links  # Return both the JSON and the list of entry.link values
+        return results
 
     except Exception as e:
         print(f"ERROR - An error occurred while processing RSS feeds: {e}")
-        return None, []
+        return {}
 
 
 if __name__ == "__main__":
+    # =============== 0. INITIAL CONFIG ===============
+    handler = CustomRequests(username=proxy_username, password=proxy_password,
+                             endpoint=proxy_server, port=proxy_port)
+    session = handler.session
 
     mongo_cnx = MongoCnx("news_db")
 
     # Query keywords search list
-    collection = mongo_cnx.db["keywords"]
-    keywords = collection.distinct("keyword", {"active": True})
-    # print("DEBUG - ", keywords)
+    collection = mongo_cnx.db['keywords']
+    query = collection.find({"active": True}, {"keyword": 1, "match_threshold": 1})
+    keyword_list = list(query)
+    # print(list(keyword_list))
 
-    # 1. FETCH URLS FROM BING API, UPDATE MONGODB
-    try:
-        # Loop keywords with `request_bing_news_urls()`
-        handler = CustomRequests(username=proxy_username, password=proxy_password,
-                                 endpoint=proxy_server, port=proxy_port)
-        session = handler.session
-        all_results_dict = {'news': []}
+    # =============== 1. FETCH URLS FROM BING API, UPDATE MONGODB ===============
+    collection = mongo_cnx.db['sources']
+    allowed_domains = collection.distinct("domain", {"active": True})
+    # print("DEBUG - allowed_domains:", allowed_domains)
 
-        for keyword in keywords:
-            response_dict, _ = request_bing_news_urls(
-                session=session, query=keyword, results_count=100, freshness="day")
-            all_results_dict['news'].extend(response_dict['news'])
+    all_results = []
 
-        result_json = json.dumps(all_results_dict, ensure_ascii=False, indent=4)
-        with open(f'./{files_path}/bing_last_results.json', 'w', encoding='utf-8') as file:
-            file.write(result_json)
+    for item in keyword_list:
+        keyword = item["keyword"]
+        result = request_bing_news_urls(session=session, query=f'"{keyword}"', freshness="month")
+        all_results.extend(result)
 
-        # Query active 'domains' on mongodb 'news_db.selectors', filter delete articles if not in list
-        collection = mongo_cnx.db["selectors"]
-        allowed_domains = collection.distinct("domain", {"active": True})
-        print("DEBUG - allowed_domains: ", allowed_domains)
+    # Filter allowed domains
+    domain_results = [item for item in all_results if item['domain'] in allowed_domains]
+    print(f"\nINFO  - Found {len(domain_results)} entries with valid domains")
+    # Filter titles for keywords
+    filtered_results = filter_news_dict(domain_results, keyword_list)
+    news_obj_list = domain_results
 
-        all_results_dict['news'] = [item for item in all_results_dict['news'] if item['domain'] in allowed_domains]
+    # Save local JSON
+    filtered_results_json = json.dumps(filtered_results, ensure_ascii=False, indent=4)
 
-        # Upsert final list on mongodb 'news_db.news'
-        mongo_cnx.insert_documents("news", all_results_dict["news"])
+    with open(f"./{files_path}/bing_last_results.json", "w", encoding="utf-8") as file:
+        file.write(filtered_results_json)
+    print(f"INFO  - Saved {len(filtered_results)} results on '{files_path}/bing_last_results.json'")
 
-    except Exception as e:
-        print("ERROR - ", e)
+    # Upsert final list on mongodb "news_db.news"
+    mongo_cnx.insert_documents("news", filtered_results)
 
-    # 2. FETCH URLS FROM WSJ RSS FEEDS, UPDATE MONGODB
-    try:
-        # List RSS feeds, loop `extract_rss_article_urls()`
-        rss_feed_urls = [
-            # "https://feeds.a.dj.com/rss/RSSOpinion.xml",
-            "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
-            "https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml",
-            "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
-            "https://feeds.a.dj.com/rss/RSSWSJD.xml",
-            # "https://feeds.a.dj.com/rss/RSSLifestyle.xml",
-        ]
+    # =============== 2. FETCH URLS FROM RSS FEEDS, UPDATE MONGODB ===============
+    collection = mongo_cnx.db['sources']
+    rss_url_list = collection.distinct("rss_list", {"active": True})
+    # print("DEBUG - ", rss_url_list)
 
-        response_dict, all_links = extract_rss_article_urls(rss_feed_urls)
+    all_results = []
 
-        result_json = json.dumps(response_dict, ensure_ascii=False, indent=4)
-        with open(f'./{files_path}/wsj_last_results.json', 'w', encoding='utf-8') as file:
-            file.write(result_json)
+    for rss_url in rss_url_list:
+        result = extract_rss_article_urls(handler, rss_url)
+        all_results.extend(result)
 
-        # Since all come from WSJ do not filter delete
-        # Upsert final list on mongodb 'news_db.news'
-        mongo_cnx.insert_documents("news", response_dict["news"])
+    # Filter titles for keywords
+    filtered_results = filter_news_dict(all_results, keyword_list)
 
-    except Exception as e:
-        print("ERROR - ", e)
+    # Save local JSON
+    filtered_results_json = json.dumps(filtered_results, ensure_ascii=False, indent=4)
+
+    with open(f"{files_path}/rss_last_results.json", "w", encoding="utf-8") as file:
+        file.write(filtered_results_json)
+    print(f"INFO  - Saved {len(filtered_results)} results on '{files_path}/rss_last_results.json'")
+
+    # Insert matches on mongodb "news_db.news"
+    mongo_cnx.insert_documents("news", filtered_results)
