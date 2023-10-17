c0561e9713803630f869ef9cf41f442986647269
Joao Brito <joao.brito@holos.company>
2023-10-17 20:18:19 -0300

    refactor: minor adjusts

--- 3_parse_summary.py
+++ 3_parse_summary.py
@@ -16,7 +16,7 @@ from src._drv_mongodb import MongoCnx
 
 # Load variables from .env
 load_dotenv()
-openai_apikey = os.getenv('OPENAI_APIKEY')
+openai_apikey = os.getenv("OPENAI_APIKEY")
 # print("DEBUG - ", proxy_username, proxy_password, proxy_server, proxy_port, wsj_username, wsj_password, bing_apikey, openai_apikey) # noqa
 
 html_files_path, json_files_path = "./html_files", "./json_files"
@@ -25,10 +25,10 @@ html_files_path, json_files_path = "./html_files", "./json_files"
 
 def clean_text(text_str):
     text_str = re.sub(r'\n', ' ', text_str)
-    text_str = ' '.join(text_str.split())
+    text_str = " ".join(text_str.split())
 
     valid_chars = set(string.printable)
-    cleaned_text = ''.join(char for char in text_str if char in valid_chars)
+    cleaned_text = "".join(char for char in text_str if char in valid_chars)
 
     return cleaned_text
 
@@ -37,46 +37,30 @@ def openai_summarize_text(input_text):
     try:
         print("INFO  - Querying OpenAI for article text summary.")
 
-        openai.api_key = os.getenv('OPENAI_APIKEY')
+        openai.api_key = os.getenv("OPENAI_APIKEY")
 
-        system_prompt = "Use a style suitable for business reports. Direct text to investors and business analysts. Write a single text block with no linkebreaks."  # noqa
-        user_prompt_1 = f"Compose a 200-word summary of the following news article text: '{input_text}'"  # noqa
+        system_prompt = "Use a style suitable for business reports with text directed to investors and \
+            business analysts. Write a single text block with no linebreaks."  # noqa
+
+        user_prompt_1 = f'Summarize the text delimited by triple quotes in about 200 words: \"\"\"{input_text}\"\"\"'  # noqa
 
         response_1 = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model="gpt-3.5-turbo-16k",
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "assistant", "content": user_prompt_1}
             ],
             temperature=0.5,
-            max_tokens=300,
+            max_tokens=320,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0,
         )
         # print("DEBUG - ", response_1)
-        summary = response_1['choices'][0]['message']['content']
-
-        # # Make a second API request
-        # user_prompt_2 = "Now generate keywords related to the subject of this same news article."
-
-        # response_2 = openai.ChatCompletion.create(
-        #     model="gpt-3.5-turbo",
-        #     messages=[
-        # {"role": "assistant", "content": summary}
-        #         {"role": "user", "content": user_prompt_2}
-        #     ],
-        #     temperature=0.5,
-        #     max_tokens=150,
-        #     top_p=1,
-        #     frequency_penalty=0,
-        #     presence_penalty=0
-        # )
-        # print("DEBUG - ", response_2)
-        # keywords = response_2['choices'][0]['message']['content']
+        summary = response_1["choices"][0]["message"]["content"]
 
         if summary:
-            print(f"INFO  - Response: '{summary[:120]}...'")
+            print(f"INFO  - Response: `{summary[:120]}...`")
 
         return summary
 
@@ -95,7 +79,7 @@ if __name__ == "__main__":
     collection_name = "news_unprocessed"
     domain = None
     start_date = None  # datetime(2023, 10, 1, 12, 00)
-    status = "content_parsed"
+    status = "content_parsed"  # or "invalid_summary" to retry errors
 
     articles = mongo_cnx.get_doc_content(collection_name=collection_name, domain=domain,
                                          start_publish_date=start_date, status=status)
@@ -106,45 +90,43 @@ if __name__ == "__main__":
 
     print("INFO  - Last document _id:", articles[-1]["_id"], ", publish_date:", articles[-1]["publish_date"])
 
-    # 3-B. Generate article summary with OpenAI
-    articles_summaries = []
+    # 3. Generate article summary with OpenAI
+    document_list = []
     total_count = len(articles)
 
     for idx, item in enumerate(articles, start=1):
-        print(f"\nINFO  - Fetching summary for document {item['_id']}.")
+        print(f'\nINFO  - {idx}/{total_count}  - fetching document {item["_id"]}, `{item["title"][0:120]}`.')
         try:
             # Send article text to openai and fetch summary
-            article_body_text = item['content']
+            article_body_text = item["content"]
             cleaned_text = clean_text(article_body_text)
+            # print(cleaned_text)  # DEBUG
 
             summary = openai_summarize_text(cleaned_text)
 
-            summary_entry = {
-                "_id": item['_id'],
+            document_entry = {
+                "_id": item["_id"],
                 "summary": summary,
                 "status": "summarized",
             }
-
-            articles_summaries.append(summary_entry)
-            print(f"INFO  - {idx}/{total_count}   - article {item['title']} summarized.")
+            document_list.append(document_entry)
 
         except Exception as e:
-            summary_entry = {
-                "_id": item['_id'],
+            document_entry = {
+                "_id": item["_id"],
                 "summary": None,
                 "status": "invalid_summary",
             }
-
-            articles_summaries.append(summary_entry)
-            print(f"ERROR - {idx}/{total_count} - Failure fetching summary for document {item['_id']}: {str(e)}")
+            document_list.append(document_entry)
+            print(f'ERROR - {idx}/{total_count} - Failure fetching summary for document {item["_id"]}:', e)
             continue  # Continue to the next item in case of an error
 
     with open(file_path, "w", encoding="utf-8") as json_file:
-        json.dump(articles_summaries, json_file)
+        json.dump(document_list, json_file)
 
     # 4. Update Mongodb with new article summaries
     with open(file_path, "r", encoding="utf-8") as json_file:
-        articles_contents = json.load(json_file)
+        document_list = json.load(json_file)
 
     mongo_cnx = MongoCnx("news_db")
-    mongo_cnx.update_collection(collection_name, articles_summaries)
+    mongo_cnx.update_collection(collection_name, document_list)
